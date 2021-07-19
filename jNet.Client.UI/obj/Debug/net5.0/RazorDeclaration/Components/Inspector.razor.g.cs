ffcf0eee774bdb7942e30f41513e3ba7382bcbf3
Scott <45034684+eeegs@users.noreply.github.com>
2021-07-19 16:52:02 +1000

    New feature

--- jNet.Client.UI/obj/Debug/net5.0/RazorDeclaration/Components/Inspector.razor.g.cs
+++ jNet.Client.UI/obj/Debug/net5.0/RazorDeclaration/Components/Inspector.razor.g.cs
@@ -12,21 +12,21 @@ namespace jNet.Client.UI
     using System.Linq;
     using System.Threading.Tasks;
 #nullable restore
-#line 1 "C:\Users\PeterGiles\source\github\jNet-Blazor\jNet.Client.UI\_Imports.razor"
+#line 1 "C:\Users\sscot\OneDrive\Development Files\Projects\jNet\jNet.Client.UI\_Imports.razor"
 using Microsoft.AspNetCore.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\PeterGiles\source\github\jNet-Blazor\jNet.Client.UI\_Imports.razor"
+#line 2 "C:\Users\sscot\OneDrive\Development Files\Projects\jNet\jNet.Client.UI\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 1 "C:\Users\PeterGiles\source\github\jNet-Blazor\jNet.Client.UI\Components\Inspector.razor"
+#line 1 "C:\Users\sscot\OneDrive\Development Files\Projects\jNet\jNet.Client.UI\Components\Inspector.razor"
 using jNet.Client.UI.Splitters;
 
 #line default
